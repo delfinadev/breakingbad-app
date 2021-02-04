f01754c6d40e479a6d2ac697356f73e31a4df356
Federico Tanco”git config --global user.email fedetanco84@gmail.comgit config --global user.name Federico <fedetanco84@gmail.com>
2021-02-04 13:11:39 -0300

    Containers and Component-2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
 import "./App.css";
 
 function App() {
-  return <div className="App">hola</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
