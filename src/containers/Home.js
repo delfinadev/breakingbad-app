f796fb9e1f727df3b43359f118bd1ecfec3f2a3f
gus <gustavo.segovia03@gmail.com>
2021-02-04 14:03:07 -0300

    api funcionando

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,21 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Home(params) {
+  const [character, setCharacter] = useState([])
+
+  useEffect(()=> {
+    let url = 'https://www.breakingbadapi.com/api/characters?name=Walter+White'
+    axios.get(url).then((response) => {
+      setCharacter(response);
+      console.log(response)
+    });
+  }, [])
+
   return (
     <>
+
+
       <h1>hola</h1>
     </>
   );
