350d54bfdb77dcb896f3a852dc97f4f1b1e06c6f
Federico Tanco”git config --global user.email fedetanco84@gmail.comgit config --global user.name Federico <fedetanco84@gmail.com>
2021-02-04 15:05:03 -0300

    APIs char y eps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Episodes from "./containers/Episodes";
+import Characters from "./containers/Characters";
 import "./App.css";
 
 function App() {
@@ -9,6 +11,12 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/episodes">
+            <Episodes />
+          </Route>
+          <Route exact path="/characters">
+            <Characters />
           </Route>
         </Switch>
       </Router>
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,21 +1,6 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-
 export default function Home(params) {
-  const [character, setCharacter] = useState([])
-
-  useEffect(()=> {
-    let url = 'https://www.breakingbadapi.com/api/characters?name=Walter+White'
-    axios.get(url).then((response) => {
-      setCharacter(response);
-      console.log(response)
-    });
-  }, [])
-
   return (
     <>
-
-
       <h1>hola</h1>
     </>
   );
